04fd001dde1d43ac4139cab5c96f214472726943
trharoldsen <travisdh@byu.edu>
2016-10-27 02:31:14 -0600

    Fixed bug in masking value in init string.  1<<64 == 1 instead of 0 -- go figure.

--- src/test/kotlin/edu/byu/ece/rapidSmith/util/luts/LutEquationTests.kt
+++ src/test/kotlin/edu/byu/ece/rapidSmith/util/luts/LutEquationTests.kt
@@ -4,11 +4,15 @@ import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DynamicTest.dynamicTest
 import java.lang.AssertionError
+import java.lang.IndexOutOfBoundsException
 import java.util.*
-import java.util.HashMap
 
 class LutEquationTests {
-	val randomEquations = Array(200, { buildRandomEquation(Random(0xDADBEBAD), 1) } )
+	val randomEquations: Array<LutEquation>
+
+	init {
+		randomEquations = Array(200, { buildRandomEquation(Random(0xDADBEBAD), 1) } )
+	}
 
 	@Test
 	@DisplayName("test constant 0")
@@ -47,28 +51,56 @@ class LutEquationTests {
 		return Array(200, {	makeLutEquationBaseTest(randomEquations[it]) }).toList()
 	}
 
-	@Test
+	@TestFactory
 	@DisplayName("test remap with empty map")
 	fun testRemapWithEmptyMap() {
-
+		val mapping = emptyMap<Int, Int>()
+		randomEquations.map { dynamicTest(it.toString(), {
+			val copy = it.deepCopy()
+			copy.remapPins(mapping)
+			compareWithRemapped(it, copy, mapping)
+		}) }
 	}
 
-	@Test
+	@TestFactory
 	@DisplayName("test direct remapping")
 	fun testDirectRemapping() {
-
+		val mapping = (1..6).associate { Pair(it, it) }
+		randomEquations.map { dynamicTest(it.toString(), {
+			val copy = it.deepCopy()
+			copy.remapPins(mapping)
+			compareWithRemapped(it, copy, mapping)
+		}) }
 	}
 
 	@TestFactory
 	@DisplayName("test fully specified remap")
 	fun testFullySpecifiedRemap() {
+		val random = Random(0x444444L)
+		randomEquations.map { dynamicTest(it.toString(), {
+			val remainingInputs = arrayListOf(1, 2, 3, 4, 5, 6)
+			Collections.shuffle(remainingInputs, random)
 
+			val mapping = (1..6).associate { Pair(it, remainingInputs.removeLast()) }
+			val copy = it.deepCopy()
+			copy.remapPins(mapping)
+			compareWithRemapped(it, copy, mapping)
+		}) }
 	}
 
-	@Test
+	@TestFactory
 	@DisplayName("test remap with partial map")
 	fun testRemapWithPartialMap() {
+		val random = Random(0x55555L)
+		randomEquations.map { dynamicTest(it.toString(), {
+			val remainingInputs = arrayListOf(1, 2, 3, 4, 5, 6)
+			Collections.shuffle(remainingInputs, random)
 
+			val mapping = (1..random.nextInt(5)+1).associate { Pair(it, remainingInputs.removeLast()) }
+			val copy = it.deepCopy()
+			copy.remapPins(mapping)
+			compareWithRemapped(it, copy, mapping)
+		}) }
 	}
 
 	// TODO move to coherence test class
@@ -122,7 +154,7 @@ private fun deepCopyUniquenessTest(equation: LutEquation) {
 }
 
 private fun isUnique(original: LutEquation, copy: LutEquation) {
-	assertEquals(original::class, copy::class)
+	assertEquals(original.javaClass, copy.javaClass)
 	return when(original) {
 		is Constant -> { /* immutable */ }
 		is LutInput -> assertNotSame(original, copy)
@@ -161,4 +193,35 @@ private fun buildRandomEquation(random: Random, depth: Int): LutEquation {
 			LutInput(input, if (nextInt % 2 == 0) true else false)
 		}
 	}
+}
+
+private fun compareWithRemapped(
+	orig: LutEquation, remapped: LutEquation, mapping: Map<Int, Int>
+) {
+	when (orig) {
+		is BinaryOperation -> {
+			assertEquals(BinaryOperation::class.java, remapped.javaClass)
+			remapped as BinaryOperation
+			compareWithRemapped(orig.left, remapped.left, mapping)
+			compareWithRemapped(orig.right, remapped.right, mapping)
+		}
+		is LutInput -> {
+			assertEquals(LutInput::class.java, remapped.javaClass)
+			remapped as LutInput
+
+			val newIndex = mapping[orig.index] ?: orig.index
+			assertEquals(newIndex, remapped.index)
+		}
+		is Constant -> {
+			assertEquals(Constant::class.java, remapped.javaClass)
+			assertSame(orig, remapped)
+		}
+	}
+}
+
+private fun <K> ArrayList<K>.removeLast(): K {
+	val lastIndex = size - 1
+	if (lastIndex < 0)
+		throw IndexOutOfBoundsException()
+	return removeAt(lastIndex)
 }
