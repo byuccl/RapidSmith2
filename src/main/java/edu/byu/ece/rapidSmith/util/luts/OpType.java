200e8821f105a755ef4154ada8c1de51fa309ce9
trharoldsen <travisdh@byu.edu>
2016-10-28 16:59:58 -0600

    Modified OpType to highlight the XDL specific portions.

--- src/main/java/edu/byu/ece/rapidSmith/util/luts/OpType.java
+++ src/main/java/edu/byu/ece/rapidSmith/util/luts/OpType.java
@@ -3,19 +3,28 @@ package edu.byu.ece.rapidSmith.util.luts;
 public enum OpType {
 	AND('*'), OR('+'), XOR('@');
 
-	public final char symbol;
+	public final char xdlSymbol;
 
-	OpType(char symbol) {
-		this.symbol = symbol;
+	OpType(char xdlSymbol) {
+		this.xdlSymbol = xdlSymbol;
 	}
 
-	public static OpType fromSymbol(char symbol) {
+	public static OpType fromXdlSymbol(char symbol) {
 		switch (symbol) {
 			case '*': return AND;
 			case '+': return OR;
 			case '@': return XOR;
 			default: throw new IllegalArgumentException("unsupported symbol");
 		}
+	}
+
+	public String toXdlString() {
+		switch (this) {
+			case AND: return "*";
+			case OR: return "+";
+			case XOR: return "@";
+		}
+		throw new AssertionError("Unknown operation");
 	}
 
 	public String toString() {
