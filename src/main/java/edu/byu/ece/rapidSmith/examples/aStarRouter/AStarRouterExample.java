9c68e4e9248a1bfaac2621118691dd77b6a507da
Thomas Townsend <thomastownsend523@gmail.com>
2017-01-17 17:38:03 -0700

    Updated routing section of the documentation

--- src/main/java/edu/byu/ece/rapidSmith/examples/aStarRouter/AStarRouterExample.java
+++ src/main/java/edu/byu/ece/rapidSmith/examples/aStarRouter/AStarRouterExample.java
@@ -3,17 +3,19 @@ import java.io.IOException;
 
 import edu.byu.ece.rapidSmith.RSEnvironment;
 import edu.byu.ece.rapidSmith.design.subsite.CellDesign;
+import edu.byu.ece.rapidSmith.design.subsite.CellNet;
 import edu.byu.ece.rapidSmith.design.subsite.RouteTree;
 import edu.byu.ece.rapidSmith.device.Device;
 import edu.byu.ece.rapidSmith.device.creation.ExtendedDeviceInfo;
 import edu.byu.ece.rapidSmith.interfaces.vivado.TincrCheckpoint;
 import edu.byu.ece.rapidSmith.interfaces.vivado.VivadoInterface;
+import edu.byu.ece.rapidSmith.util.DotFilePrinter;
 import edu.byu.ece.rapidSmith.util.RapidSmithDebug;
 
 /**
  * Demonstrates the sample {@link AStarRouter} on a single net in a CORDIC design. 
  * To see the results of the router, open up the equivalent cordic.dcp file in Vivado,
- * and run the hightlight TCL command that is produced after running this program. This
+ * and run the highlight TCL command that is produced after running this program. This
  * file also demonstrates how to load the extended device information that is not
  * included by default (reverse wire connections for example).
  * 
@@ -43,7 +45,9 @@ public class AStarRouterExample {
 		// Routing net
 		System.out.println("Routing Net...");
 		AStarRouter router = new AStarRouter();
-		RouteTree test = router.routeNet(design.getNet("u2/gen_pipe[8].Pipe/Zo_reg_n_0_[9]"));
+		CellNet net = design.getNet("u2/gen_pipe[8].Pipe/Zo_reg_n_0_[9]");
+		RouteTree test = router.routeNet(net);
+		net.addIntersiteRouteTree(test);
 		
 		// Displaying results
 		System.out.println("\nCommand to highlight chosen wires in Vivado: ");
@@ -51,5 +55,8 @@ public class AStarRouterExample {
 		
 		System.out.println("\nRouteTree data structure in RapidSmith:");
 		RapidSmithDebug.printRouteTree(test);
+		
+		System.out.println("\nRouteTree data structure in DOT file format");
+		System.out.println("\n" + DotFilePrinter.getRouteTreeDotString(net));
 	}
 }
