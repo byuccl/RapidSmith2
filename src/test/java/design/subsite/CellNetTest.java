8a5a1b868e226f4c944790508c79c1fb8eaf1288
Mark Crossen <markcrossen@studentbody.byu.edu>
2017-03-25 14:14:33 -0600

    test CellNet 'isClkNet' method

--- src/test/java/design/subsite/CellNetTest.java
+++ src/test/java/design/subsite/CellNetTest.java
@@ -21,24 +21,14 @@
 package design.subsite;
 import java.io.IOException;
 import java.util.Collection;
-import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.stream.Collectors;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.junit.platform.runner.JUnitPlatform;
 import static org.junit.jupiter.api.Assertions.*;
 import edu.byu.ece.rapidSmith.RSEnvironment;
-import edu.byu.ece.rapidSmith.device.Device;
-import edu.byu.ece.rapidSmith.device.PortDirection;
 import edu.byu.ece.rapidSmith.device.PinDirection;
-import edu.byu.ece.rapidSmith.device.BelId;
-import edu.byu.ece.rapidSmith.device.Site;
-import edu.byu.ece.rapidSmith.device.Bel;
-import edu.byu.ece.rapidSmith.design.subsite.CellDesign;
 import edu.byu.ece.rapidSmith.design.subsite.CellLibrary;
 import edu.byu.ece.rapidSmith.design.subsite.Cell;
 import edu.byu.ece.rapidSmith.design.subsite.CellPin;
@@ -72,18 +62,69 @@ class CellNetTests {
     }
 
     @Test
+    @DisplayName("test cellNet 'isClkNet' method")
+    void testIsClkNet() {
+        // create the nets and cells that will be used by this test
+        CellNet clk_net = new CellNet("clk_net", NetType.WIRE);
+        CellNet non_clk_net = new CellNet("non_clk_net", NetType.WIRE);
+        CellNet mix_net = new CellNet("mix_net", NetType.WIRE);
+        Cell cell = new Cell("test_cell", cell_library.get("FDRE"));
+        Cell cell2 = new Cell("test_cell2", cell_library.get("FDRE"));
+
+        // connect and disconnect a clock pin and check isClkNet
+        assertFalse(clk_net.isClkNet(), "Net shouldn't be a ClkNet before connecting to a clock pin.");
+        clk_net.connectToPin(cell.getPin("C"));
+        assertTrue(clk_net.isClkNet(), "Net should be a ClkNet after connecting to a clock pin.");
+        clk_net.disconnectFromPin(cell.getPin("C"));
+        assertFalse(clk_net.isClkNet(), "Net shouldn't be a ClkNet after disconnecting from a clock pin.");
+
+        // connect and disconnect a non clock pin and check isClkNet
+        assertFalse(non_clk_net.isClkNet(), "Net shouldn't be a ClkNet without any pins.");
+        non_clk_net.connectToPin(cell.getPin("D"));
+        assertFalse(non_clk_net.isClkNet(), "Net shouldn't be a ClkNet with only non clock pins.");
+        non_clk_net.disconnectFromPin(cell.getPin("D"));
+        assertFalse(non_clk_net.isClkNet(), "Net shouldn't be a ClkNet after removing all pins.");
+
+        // connect a non clock pin, then connect a clock pin. verify isClkNet at each step
+        assertFalse(mix_net.isClkNet(), "Net shouldn't be a ClkNet before connecting any pins.");
+        mix_net.connectToPin(cell.getPin("D"));
+        assertFalse(mix_net.isClkNet(), "Net shouldn't be a ClkNet after connecting a non clock pin.");
+        mix_net.connectToPin(cell.getPin("C"));
+        assertTrue(mix_net.isClkNet(), "Net should be a ClkNet after connecting at least one clock pin.");
+        // connect and disconnect a non clock pin
+        mix_net.connectToPin(cell.getPin("Q"));
+        assertTrue(mix_net.isClkNet(), "Net should still be a ClkNet after connecting another non-clock pin.");
+        mix_net.disconnectFromPin(cell.getPin("D"));
+        assertTrue(mix_net.isClkNet(), "Net should still be a ClkNet after disconnecting a non-clock pin.");
+        // connect and disconnect another clock pin
+        mix_net.connectToPin(cell2.getPin("C"));
+        assertTrue(mix_net.isClkNet(), "Net should still be a ClkNet after connecting another clock pin.");
+        mix_net.disconnectFromPin(cell.getPin("C"));
+        // disconnect all pins
+        assertTrue(mix_net.isClkNet(), "Net should still be a ClkNet if another clock pin is still connected.");
+        mix_net.disconnectFromPin(cell2.getPin("C"));
+        assertFalse(mix_net.isClkNet(), "Net shouldn't be a ClkNet after disconnecting all clock pins.");
+        mix_net.disconnectFromPin(cell.getPin("Q"));
+        assertFalse(mix_net.isClkNet(), "Net shouldn't be a ClkNet after removing all pins.");
+    }
+
+    @Test
     @DisplayName("test CellNet 'isConnectedToPin' method")
     void testIsConnectedToPin() {
         CellNet net = new CellNet("test_net", NetType.WIRE);
         Cell cell = new Cell("test_cell", cell_library.get("LUT3"));
         List<CellPin> pins = new ArrayList<CellPin>(cell.getPins());
+        // slowly connect all the pins from the test cell
         for (int index = 0; index < pins.size(); index++) {
             net.connectToPin(pins.get(index));
+            // verify the right pin and pin size are connected
             assertTrue(net.isConnectedToPin(pins.get(index)), "CellNet not connected to pin after adding it");
             assertEquals(index+1, net.getPins().size(), "CellNet has improper pin count after adding a new pin.");
         }
+        // slowly disconnect all the pins on the net
         for (int index = 0; index < pins.size(); index++) {
             net.disconnectFromPin(pins.get(index));
+            // verify the right pin and pin size are connected
             assertFalse(net.isConnectedToPin(pins.get(index)), "CellNet still connected to pin after being removed.");
             assertEquals(pins.size()-index-1, net.getPins().size(), "CellNet has improper pin count after removing a pin.");
         }
@@ -95,16 +136,21 @@ class CellNetTests {
         Cell lut3_cell = new Cell("test_lut", cell_library.get("LUT3"));
         Cell ioport_cell = new Cell("test_inout", cell_library.get("IOPORT"));
         CellNet net = new CellNet("test_net", NetType.WIRE);
+        // connect several inputs, outputs, and inoutputs to the net
         net.connectToPins(lut3_cell.getPins());
         net.connectToPins(ioport_cell.getPins());
+        // verify that the net has a source pin (LUT3-O)
         assertNotNull(net.getSourcePin(), "Net has no source pin.");
         assertEquals(lut3_cell.getPin("O"), net.getSourcePin(), "Net has improper source pin.");
+        // verify the net has the right source pins (LUT3-O, IOPORT-PAD)
         assertEquals(lut3_cell.getOutputPins().size() + ioport_cell.getPins().size(), net.getAllSourcePins().size(), "Net has improper source pin count");
         verify_collection(lut3_cell.getOutputPins(), net.getAllSourcePins(), "CellNet", "source pin", true);
         verify_collection(ioport_cell.getPins(), net.getAllSourcePins(), "CellNet", "source pin", true);
+        // verify the net has the right sink pins (LUT3-I0, LUT3-I1, LUT3-I2, IOPORT-PAD)
         assertEquals(lut3_cell.getInputPins().size() + ioport_cell.getPins().size(), net.getSinkPins().size(), "Net has improper sink pin count");
         verify_collection(lut3_cell.getInputPins(), net.getSinkPins(), "CellNet", "sink pin", true);
         verify_collection(ioport_cell.getPins(), net.getSinkPins(), "CellNet", "sink pin", true);
+        // disconnect the source pin and verify that it changes to the other source pin
         net.disconnectFromPin(lut3_cell.getPin("O"));
         assertEquals(ioport_cell.getPin("PAD"), net.getSourcePin(), "Net has improper source pin after removing original source pin");
         verify_collection(ioport_cell.getPins(), net.getAllSourcePins(), "CellNet", "source pin", false);
@@ -133,6 +179,7 @@ class CellNetTests {
         CellNet net = new CellNet("test_net", NetType.WIRE);
         Cell cell = new Cell("test_cell", cell_library.get("LUT5"));
         CellPin pin = cell.attachPseudoPin("test_pin", PinDirection.INOUT);
+        // connect and then disconnect a pseudo pin to the net. verify the return value of getPseudoPinCount
         net.connectToPin(pin);
         assertEquals(1, net.getPseudoPinCount(), "Net returns wrong pseudo pin count after connecting a pseudo pin");
         net.disconnectFromPin(pin);
@@ -144,8 +191,10 @@ class CellNetTests {
     void testDetachNet() {
         CellNet net = new CellNet("test_net", NetType.WIRE);
         Cell cell = new Cell("test_cell", cell_library.get("LUT3"));
+        // attach a lot of pins
         net.connectToPins(cell.getPins());
         assertEquals(cell.getPins().size(), net.getPins().size(), "Net didn't connect to pins correctly");
+        // disconnect all the pins
         net.detachNet();
         assertEquals(0, net.getPins().size(), "Net didn't detach from pins correctly");
     }
@@ -155,12 +204,16 @@ class CellNetTests {
     void testGetFanOut() {
         CellNet net = new CellNet("test_net", NetType.WIRE);
         List<CellPin> sink_pins = new ArrayList<CellPin>(new Cell("test_cell", cell_library.get("LUT3")).getInputPins());
+        // slowly attach sink pins to the net
         for (int index = 0; index < sink_pins.size(); index++) {
             net.connectToPin(sink_pins.get(index));
+            // verify the fan out
             assertEquals(index+1, net.getFanOut(), "CellNet has improper fan out after adding a new sink pin.");
         }
+        // slowly disconnect sink pins from the net
         for (int index = 0; index < sink_pins.size(); index++) {
             net.disconnectFromPin(sink_pins.get(index));
+            // verify the fan out
             assertEquals(sink_pins.size()-index-1, net.getFanOut(), "CellNet has improper fan out after removing a sink pin.");
         }
     }
