124ebe526df55ea996fdf2e22c504113a4adeed8
Brent Nelson <nelson@ee.byu.edu>
2018-01-26 00:34:24 -0700

    Removed built-in properties list from script

--- devices/artix7/pinMappings/create_nondefault_pin_mappings.tcl
+++ devices/artix7/pinMappings/create_nondefault_pin_mappings.tcl
@@ -5,14 +5,6 @@ set libcell [get_lib_cells $libcellname]
 set cell [create_cell -reference $libcell "tmpcell"]
 set bel [lindex [get_bels *$belname] 0]
 
-# Create dictionary of changes to apply
-set config_dict [dict create]
-dict set config_dict READ_WIDTH_A 2
-dict set config_dict READ_WIDTH_B 4
-dict set config_dict WRITE_WIDTH_A 2
-dict set config_dict WRITE_WIDTH_B 4
-dict set config_dict  RAM_MODE TDP
-
 # Create the actual pin mappings
 set pin_mappings [tincr::cells::create_nondefault_pin_mappings $cell $bel $config_dict] 
 #dict for {pin mapping} $pin_mappings {
