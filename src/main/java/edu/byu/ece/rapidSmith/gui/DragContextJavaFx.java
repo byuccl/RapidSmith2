53c21fdce344db3daaf80201ec1703a14d186811
grigg3 <grigg.jesse@gmail.com>
2018-06-18 13:23:32 -0600

    Explored different solution that would provide individual wire selection functionality

--- src/main/java/edu/byu/ece/rapidSmith/gui/DragContextJavaFx.java
+++ src/main/java/edu/byu/ece/rapidSmith/gui/DragContextJavaFx.java
@@ -1,4 +1,9 @@
 package edu.byu.ece.rapidSmith.gui;
 
 public class DragContextJavaFx {
+        double mouseAnchorX;
+        double mouseAnchorY;
+
+        double translateAnchorX;
+        double translateAnchorY;
 }
