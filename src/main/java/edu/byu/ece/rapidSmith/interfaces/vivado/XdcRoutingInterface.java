54bd5233a2e5ab77c1d8afcf5f34578c9f5ef98f
tharoldsen <tharoldsen@isi.edu>
2018-11-09 16:26:03 -0500

    Fixed NPE when net of sinkCellPin is null in xdc importer

--- src/main/java/edu/byu/ece/rapidSmith/interfaces/vivado/XdcRoutingInterface.java
+++ src/main/java/edu/byu/ece/rapidSmith/interfaces/vivado/XdcRoutingInterface.java
@@ -1243,7 +1243,7 @@ public class XdcRoutingInterface {
 				}
 			}
 			else {
-				if (sinkCellPin == null || !sinkCellPin.getNet().equals(this.net)) {
+				if (sinkCellPin == null || !Objects.equals(sinkCellPin.getNet(), this.net)) {
 					return false;
 				}
 			}
